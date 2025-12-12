56bed262eb2e3706ab2364ffeec667910c1b2587
you-love-lucy <fjones96@ivytech.edu>
2025-12-11 20:23:08 -0500

    Update ticket.py
    added endRental method to calculate total time used, total fee to be charged, and set the bike status to avaliable. also added toDict and fromDict methods to ensure JSON compatibility.

--- src/ticket.py
+++ src/ticket.py
@@ -10,6 +10,48 @@ class Ticket:
         self.end_time = end_time
         self.total_fee = total_fee
 
+        # mark the bike rented
+        self.bike.mark_rented()
+
     def __repr__(self):
         return (f"Ticket({self.id}, Bike {self.bike.id}, Customer {self.customer.id}, "
                 f"Start: {self.start_time}, End: {self.end_time}, Fee: ${self.total_fee})")
+
+    def toDict(self):
+        # convert Ticket object into a dictonary for JSON compatibility
+        return {
+            "id": self.id,
+            "bike_id": self.bike.id,
+            "customer_id": self.customer.id,
+            "start_time": self.start_time.isoformat() if self.start_time else None,
+            "planned_hours": self.planned_hours,
+            "end_time": self.end_time.isoformat() if self.end_time else None,
+            "total_fee": self.total_fee
+        }
+    
+    def endRental(self):
+        # get current time
+        self.end_time = datetime.now()
+
+        # get the difference between the start and end times
+        durationSec = (self.start_time - self.end_time).total_seconds()
+        duration = durationSec/3600
+
+        # calculate and round the fee
+        fee = (duration*self.bike.hourly_rate)
+        self.total_fee = round(fee, 2)
+
+        # mark the bike avaliable again
+        self.bike.markAvaliable()
+
+        return self.total_fee
+    
+    @classmethod
+    def fromDict(cls, data, bikes, customers):
+        # get info from dictionary to turn into a Ticket object with that info
+        bike = bikes[data['bike_id']]
+        customer = customers[data['customer_id']]
+        start_time = datetime.fromisoformat(data['state_time']) if data['start_time'] else None
+        end_time = datetime.fromisoformat(data['end_time']) if data['end_time'] else None
+
+        return cls(id=data['id'], bike=bike, customer=customer, start_time=start_time, planned_hours=data['planned_hours'], end_time=end_time, total_fee=data['total_fee'])
